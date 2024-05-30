749d3b52ea0bf1d8acafcff52a95e27bbc6700c3
Michael Bottini <mbottini1@gmail.com>
2024-05-29 22:08:37 -0700

    Fix access specifiers and doc comments for Geometry

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,23 +1,23 @@
 use crate::material::Material;
 use crate::piece_materials::PieceMaterials;
 
-/// Geometry of the piece being simulated.
-/// a: x-direction distance where core becomes reflector
-/// b: y-direction distance where cores transition
-/// l_x: x-direction total distance
-/// l_y: y-direction total distance
-/// materials: Material coefficients of each part of the piece
+/// Parameters for representing the geometry of a simulation piece.
 pub struct Geometry {
-    a: f64,
-    b: f64,
-    l_x: f64,
-    l_y: f64,
-    materials: PieceMaterials,
+    /// X-direction distance where core becomes reflector.
+    pub a: f64,
+    /// Y-direction distance where cores transition.
+    pub b: f64,
+    /// X-direction total distance.
+    pub l_x: f64,
+    /// Y-direction total distance.
+    pub l_y: f64,
+    /// Material coefficients of each part of the piece.
+    pub materials: PieceMaterials,
 }
 
 impl Geometry {
     /// Given x and y coordinates, return the material properties of that location.
-    fn get_material(&self, x: f64, y: f64) -> &Material {
+    pub fn get_material(&self, x: f64, y: f64) -> &Material {
         if x > self.a {
             &self.materials.reflector
         } else if y > self.b {
